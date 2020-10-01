6a3dd2807156cd5d8224f41d28d1ff482c6eb1fe
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    sm8350-common: Remove packages at build time
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

---
+++ remove_packages/Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    com.android.hotwordenrollment.common.util \
+    AudioFX \
+    Browser2 \
+    Calendar \
+    Etar \
+    ExactCalculator \
+    Gallery2 \
+    Glimpse \
+    HotwordEnrollmentOKGoogleEx3CORTEXM4 \
+    HotwordEnrollmentXGoogleEx3CORTEXM4 \
+    Jelly \
+    MatLog \
+    Music \
+    MusicFX \
+    SystemUpdater \
+    Twelve \
+    Updater
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
